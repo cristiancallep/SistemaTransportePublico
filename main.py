dc8bce369d02e953cfb6805e77b68b63d8834a31
emely <melyloaiza2007@gmail.com>
2025-09-02 17:42:50 -0500

    Feat: correcciones desde la ultima entrega

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from src.Gestor import GestorTarjeta
-from src.Transporte import  Tranvia, Bus, Metro
+from src.Transporte import Transporte
+from src.Tranvia import  Tranvia
+from src.Bus import  Bus
+from src.Metro import Metro
 
 gestor = GestorTarjeta()
 tranvia = Tranvia("A", 200, 12)  # Linea, capacidad, paradas
@@ -7,16 +10,25 @@ bus = Bus(30, 4, "9:00 - 22:00", "Electrico")  # Linea, capacidad, paradas
 metro = Metro("J", 10, "5:00 - 20:00")  # Linea, capacidad, paradas
 
 def menu():
+    """
+    Función principal que muestra el menú y maneja las opciones del usuario.
+    Permite crear tarjetas, recargar saldo, consultar tarjetas y comprar tiquetes para diferentes medios de transporte.
+    Args:
+        None    
+    Returns:
+        None
+        
+    """
     while True:
-        print("\n--- 🚏 Sistema de Transporte Público 🚏 ---")
-        print("🔶 1. Conseguir Tarjeta")
-        print("🔶 2. Recargar tarjeta")
-        print("🔶 3. Consultar tarjetas")
-        print("🔶 4. Consultar saldo")
-        print("🔶 5. Comprar tiquete en Bus")
-        print("🔶 6. Comprar tiquete en Metro")
-        print("🔶 7. Comprar tiquete en Tranvía")
-        print("🔶 8. Salir")
+        print("\n--- Sistema de Transporte Público  ---")
+        print("1. Conseguir Tarjeta")
+        print("2. Recargar tarjeta")
+        print("3. Consultar tarjetas")
+        print("4. Consultar saldo")
+        print("5. Comprar tiquete en Bus")
+        print("6. Comprar tiquete en Metro")
+        print("7. Comprar tiquete en Tranvía")
+        print("8. Salir")
 
         opcion = input("Seleccione una opción: ").strip()
 
@@ -26,12 +38,12 @@ def menu():
             
             # Validación de datos
             if nombre.isalpha() == False or documento.isdigit() == False:#validacion de datos validos
-                print("❌ Nombre o documento inválido.")
+                print("Nombre o documento inválido.")
                 continue
             if gestor.crear_tarjeta(nombre, documento):
-                print(f"✅ Tarjeta creada para {nombre}")
+                print(f"Tarjeta creada para {nombre}")
             else:
-                print("❌ Ya existe una tarjeta registrada para este documento")
+                print("Ya existe una tarjeta registrada para este documento")
 
         elif opcion == "2": # Opción para recargar una tarjeta existente
             documento = input("Ingrese su documento: ").strip()
@@ -42,60 +54,54 @@ def menu():
                 if monto > 0:
                     if gestor.recargar(documento, monto):
                         saldo = gestor.consultar(documento)
-                        print(f"✅ Recarga exitosa, nuevo saldo: {saldo}")
+                        print(f"Recarga exitosa, nuevo saldo: {saldo}")
                     else:
-                        print("❌ No hay ninguna tarjeta asociada a ese documento")
+                        print("No hay ninguna tarjeta asociada a ese documento")
                 else:
-                    print("❌ Monto inválido para recarga.")
+                    print("Monto inválido para recarga.")
             else:
-                print("❌ Debe ingresar solo números enteros.")
+                print("Debe ingresar solo números enteros.")
 
         elif opcion == "3": # Opción para consultar todas las tarjetas registradas
-            if gestor.get_targetas() == {}:
-                print("❌ No hay tarjetas registradas.")
-                continue
-            else:
-                print("💳 Tarjetas registradas: 💳")
-                for doc, tarjeta in gestor.get_targetas().items():
-                    print(f"Nombre: {tarjeta.get_nombre()}\nSaldo: {tarjeta.get_saldo()} \n -------")
-        
+            gestor.mostrar_tarjetas()
+            
         elif opcion == "4": # Opción para consultar el saldo de una tarjeta
             documento = input("Ingrese su documento: ").strip()
             saldo = gestor.consultar(documento)
             if saldo == 0:
-                print("❌ No hay saldo asociado a este documento.")
+                print("No hay saldo asociado a este documento.")
             else:
-                print(f"💳 Su saldo es: {saldo}")
+                print(f"Su saldo es: {saldo}")
 
         elif opcion == "5":# Comprar tiquete para Bus
             documento = input("Ingrese su documento: ").strip()
             if gestor.vender_tiquete(documento, bus):
-                print("🎟️  Tiquete de Bus comprado con éxito.")
-                print("ℹ️  ", bus.info())
+                print("Tiquete de Bus comprado con éxito.")
+                print("*", bus.info())
             else:
-                print("❌ No se pudo comprar: saldo insuficiente o tarjeta inexistente.")
+                print(" No se pudo comprar: saldo insuficiente o tarjeta inexistente.")
 
         elif opcion == "6": # Comprar tiquete para Metro
             documento = input("Ingrese su documento: ").strip()
             if gestor.vender_tiquete(documento, metro):
-                print("🎟️   Tiquete de Metro comprado con éxito.")
-                print("ℹ️  ", metro.info())
+                print("Tiquete de Metro comprado con éxito.")
+                print("*", metro.info())
             else:
-                print("❌ No se pudo comprar: saldo insuficiente o tarjeta inexistente.")
+                print("No se pudo comprar: saldo insuficiente o tarjeta inexistente.")
 
         elif opcion == "7":# Comprar tiquete para Tranvía
             documento = input("Ingrese su documento: ").strip()
             if gestor.vender_tiquete(documento, tranvia):
-                print("🎟️   Tiquete de Tranvía comprado con éxito.")
-                print("ℹ️  ", tranvia.info())
+                print("Tiquete de Tranvía comprado con éxito.")
+                print("*", tranvia.info())
             else:
-                print("❌ No se pudo comprar: saldo insuficiente o tarjeta inexistente.")
+                print("No se pudo comprar: saldo insuficiente o tarjeta inexistente.")
 
         elif opcion == "8": # Salir del sistema
-            print("👋 Gracias por usar el sistema de transporte.")
+            print("Gracias por usar el sistema de transporte.")
             break
         else:
-            print("❌ Opción inválida. Intente de nuevo.")
+            print("Opción inválida. Intente de nuevo.")
 
 
 if __name__ == "__main__":
